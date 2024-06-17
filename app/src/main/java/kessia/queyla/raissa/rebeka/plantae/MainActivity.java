dbaf3618a05e94744fbce33d20b1a637341ed110
Rebeka_Queyla <diasbekadias@gmail.com>
2024-06-17 13:38:09 -0300

    start

---
+++ app/src/main/java/kessia/queyla/raissa/rebeka/plantae/MainActivity.java
@@ -0,0 +1,16 @@
+package kessia.queyla.raissa.rebeka.plantae;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
