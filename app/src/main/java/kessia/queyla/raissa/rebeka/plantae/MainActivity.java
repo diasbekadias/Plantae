5c3af462a93b102b9171ebe0b6ec925092bd4392
rebeka <rebekadiasdias2020@gmail.com>
2024-06-20 11:14:07 -0300

    adicionar botao layout2 problemas ao adicionar foto

--- app/src/main/java/kessia/queyla/raissa/rebeka/plantae/MainActivity.java
+++ app/src/main/java/kessia/queyla/raissa/rebeka/plantae/MainActivity.java
@@ -1,16 +1,27 @@
 package kessia.queyla.raissa.rebeka.plantae;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 2000; // Duração da tela de logo (2 segundos)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Handler para iniciar a AddPhotoActivity após alguns segundos
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this, AddPhotoActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 }
